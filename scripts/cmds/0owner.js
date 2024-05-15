393f83f4153e8a875eff76fd8b7261d12c977f89
Lorenzo <154640621+lorenzoNeedyou@users.noreply.github.com>
2024-05-15 08:18:45 +0800

    Update 0owner.js

--- scripts/cmds/0owner.js
+++ scripts/cmds/0owner.js
@@ -16,12 +16,12 @@ config: {
   onStart: async function ({ api, event }) {
   try {
     const ownerInfo = {
-      name: 'Yazky Ai',
+      name: 'Lorenzo Ai',
       gender: 'Male',
       age: 'Unknown',
       height: 'Unknown',
       facebookLink: 'stalk moko mwa',
-      nick: 'Yazky'
+      nick: 'Lorenz'
     };
 
     const bold = 'https://i.imgur.com/DDO686J.mp4'; // Replace with your Google Drive videoid link https://drive.google.com/uc?export=download&id=here put your video id
